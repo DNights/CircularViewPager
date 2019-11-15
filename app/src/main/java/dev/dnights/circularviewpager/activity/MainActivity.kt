44e1e1fba332be6cdeaa30e99796b780c5a9cc2c
hyundonghan <guehgk456@gmail.com>
2019-11-15 10:50:18 +0900

    fix sample code in MainActivity

--- app/src/main/java/dev/dnights/circularviewpager/activity/MainActivity.kt
+++ app/src/main/java/dev/dnights/circularviewpager/activity/MainActivity.kt
@@ -2,6 +2,7 @@ package dev.dnights.circularviewpager.activity
 
 import android.os.Bundle
 import android.view.View
+import androidx.viewpager.widget.ViewPager
 import com.bumptech.glide.Glide
 import com.dnights.restfullapisampletest.api.API
 import com.dnights.restfullapisampletest.api.AccessKey
@@ -43,6 +44,37 @@ class MainActivity : BaseActivity() {
                 return R.layout.item_imageview
             }
         })
+
+        mainViewPager.addOnPageChangeListener(object: ViewPager.OnPageChangeListener{
+
+            private var pagerPosition = -1
+
+            override fun onPageScrollStateChanged(state: Int) {
+                if (state == ViewPager.SCROLL_STATE_IDLE) {
+                    val pageCount = mainViewPager.adapter!!.count
+                    if (pageCount == 1) {
+                        return
+                    }
+                    if (pagerPosition == 0) {
+                        mainViewPager.setCurrentItem(pageCount - 2, false)
+                    } else if (pagerPosition == pageCount - 1) {
+                        mainViewPager.setCurrentItem(1, false)
+                    }
+                }
+            }
+
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+
+            }
+
+            override fun onPageSelected(position: Int) {
+                pagerPosition = position
+            }
+        })
     }
 
     private fun callApiWithPhoto() {
@@ -53,8 +85,7 @@ class MainActivity : BaseActivity() {
                 {
                     listPhoto = it.body()!!
                     mainViewPager.adapter!!.notifyDataSetChanged()
-                    mainViewPager.setCurrentItem(0, false)
-                    mainViewPager.setCurrentItem(1, false)
+                    mainViewPager.pageCount = listPhoto.size
                 }, {
 
                 }
