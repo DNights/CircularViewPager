43ac1a8b1bb824282e7df5284490f25951a989a6
hyundonghan <guehgk456@gmail.com>
2019-11-15 14:15:12 +0900

    remove mainViewPager.addOnPageChangeListener

--- app/src/main/java/dev/dnights/circularviewpager/activity/MainActivity.kt
+++ app/src/main/java/dev/dnights/circularviewpager/activity/MainActivity.kt
@@ -44,37 +44,6 @@ class MainActivity : BaseActivity() {
                 return R.layout.item_imageview
             }
         })
-
-        mainViewPager.addOnPageChangeListener(object: ViewPager.OnPageChangeListener{
-
-            private var pagerPosition = -1
-
-            override fun onPageScrollStateChanged(state: Int) {
-                if (state == ViewPager.SCROLL_STATE_IDLE) {
-                    val pageCount = mainViewPager.adapter!!.count
-                    if (pageCount == 1) {
-                        return
-                    }
-                    if (pagerPosition == 0) {
-                        mainViewPager.setCurrentItem(pageCount - 2, false)
-                    } else if (pagerPosition == pageCount - 1) {
-                        mainViewPager.setCurrentItem(1, false)
-                    }
-                }
-            }
-
-            override fun onPageScrolled(
-                position: Int,
-                positionOffset: Float,
-                positionOffsetPixels: Int
-            ) {
-
-            }
-
-            override fun onPageSelected(position: Int) {
-                pagerPosition = position
-            }
-        })
     }
 
     private fun callApiWithPhoto() {
